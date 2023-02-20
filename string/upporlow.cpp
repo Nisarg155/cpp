dba97ded9f400a1fdfed351664d32aa534e9fd15
Nisarg <amlaninisarg15@gmail.com>
2023-02-20 15:19:06 +0530

    modified upperlowercase

--- string/upporlow.cpp
+++ string/upporlow.cpp
@@ -1,12 +1,12 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-string str;
-getline(cin,str);
-int freq[26] = {0};
-for (int i = 0; i < str.length(); i++)
-    freq[str[i] - 'a']++;
+    string str;
+    getline(cin, str);
+    int freq[26] = {0};
+    for (int i = 0; i < str.length(); i++)
+        freq[str[i] - 'a']++;
 
-return 0;
+    return 0;
 }
